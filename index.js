79989a2b1b9596ca2c8a70a3a9500c81fc44abbe
biscuitG <purushottamjha447@gmail.com>
2023-04-18 17:30:10 +0530

    JS basics

---
+++ index.js
@@ -0,0 +1,36 @@
+//BASICS OF JS
+
+// alert(anything ) output in browser
+// prompmt (anthing ) in browser
+// _,alphabet,$ se start variable
+// 1) LET VAR CONST
+
+let a=23;
+a=24;
+console.log(a);
+a=`hi`;
+b="Hello";
+console.log(`Hi sir ${a} and ${b} `);
+// ${} only work in `  ` not " "
+// Syntax error as defined already let a=24; 
+
+
+
+var b=45;
+var b=78;
+console.log(b); //78 not SynError see how JS execute
+var b='hilo';
+console.log(b);// hilo
+
+const c=45;// only way of declaring it 
+// TypeError ,c=47;
+console.log(c);
+// const d;  Error mus initialse
+// console.log(d);
+
+
+
+
+
+
+
